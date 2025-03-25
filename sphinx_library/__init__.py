e87dd06d235a4cea1e5c7e1546613b51aa462d54
Vince Salvino <salvino@coderedcorp.com>
2025-03-25 16:54:11 -0400

    Switch to pyproject.toml and ruff

--- sphinx_library/__init__.py
+++ sphinx_library/__init__.py
@@ -4,6 +4,5 @@ import os
 def setup(app):
     if hasattr(app, "add_html_theme"):
         app.add_html_theme(
-            "library",
-            os.path.abspath(os.path.dirname(__file__))
+            "library", os.path.abspath(os.path.dirname(__file__))
         )
--- sphinx_library/theme.py
+++ sphinx_library/theme.py
@@ -1,22 +1,12 @@
-# flake8: noqa
-
 from pygments.style import Style
-
-from pygments.token import (
-    Keyword,
-    Name,
-    Comment,
-    String,
-    Error,
-    Number,
-    Operator,
-    Generic,
-    Whitespace,
-    Punctuation,
-    Other,
-    Literal,
-    Text
-)
+from pygments.token import Comment
+from pygments.token import Error
+from pygments.token import Generic
+from pygments.token import Keyword
+from pygments.token import Name
+from pygments.token import Number
+from pygments.token import Operator
+from pygments.token import String
 
 
 class LibraryLight(Style):
@@ -24,26 +14,26 @@ class LibraryLight(Style):
     default_style = None
 
     styles = {
-        Comment:            "italic #777",
-        Keyword:            "bold",
-        Keyword.Type:       "#458",
-        Name.Builtin:       "#458",
-        Name.Constant:      "#458",
-        Name.Class:         "bold #458",
-        Name.Function:      "bold #844",
-        Name.Exception:     "bold #844",
-        Name.Variable:      "#458",
-        Number:             "#099",
-        Operator:           "bold",
-        String:             "#a84",
-        String.Doc:         "italic #777",
-        Generic.Error:      '#a00',
-        Generic.Emph:       'italic',
-        Generic.Strong:     'bold',
-        Generic.Prompt:     '#555',
-        Generic.Output:     '#777',
-        Generic.Traceback:  '#a00',
-        Error:              'bg:#e3d2d2 #a61717'
+        Comment: "italic #777",
+        Keyword: "bold",
+        Keyword.Type: "#458",
+        Name.Builtin: "#458",
+        Name.Constant: "#458",
+        Name.Class: "bold #458",
+        Name.Function: "bold #844",
+        Name.Exception: "bold #844",
+        Name.Variable: "#458",
+        Number: "#099",
+        Operator: "bold",
+        String: "#a84",
+        String.Doc: "italic #777",
+        Generic.Error: "#a00",
+        Generic.Emph: "italic",
+        Generic.Strong: "bold",
+        Generic.Prompt: "#555",
+        Generic.Output: "#777",
+        Generic.Traceback: "#a00",
+        Error: "bg:#e3d2d2 #a61717",
     }
 
 
@@ -52,18 +42,20 @@ class LibraryDark(LibraryLight):
     default_style = None
 
     styles = LibraryLight.styles
-    styles.update({
-        Comment:            "italic #aaa",
-        Keyword.Type:       "#9ad",
-        Name.Builtin:       "#9ad",
-        Name.Constant:      "#9ad",
-        Name.Class:         "bold #9ad",
-        Name.Function:      "bold #c88",
-        Name.Exception:     "bold #c88",
-        Name.Variable:      "#9ad",
-        Number:             "#8cc",
-        String:             "#ca8",
-        String.Doc:         "italic #aaa",
-        Generic.Prompt:     '#888',
-        Generic.Output:     '#aaa',
-    })
+    styles.update(
+        {
+            Comment: "italic #aaa",
+            Keyword.Type: "#9ad",
+            Name.Builtin: "#9ad",
+            Name.Constant: "#9ad",
+            Name.Class: "bold #9ad",
+            Name.Function: "bold #c88",
+            Name.Exception: "bold #c88",
+            Name.Variable: "#9ad",
+            Number: "#8cc",
+            String: "#ca8",
+            String.Doc: "italic #aaa",
+            Generic.Prompt: "#888",
+            Generic.Output: "#aaa",
+        }
+    )
